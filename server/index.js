329db51112c3a2bcad3e6ae8db720a58b3b8c367
sanjay-jupakula <80062669+sanjay-jupakula@users.noreply.github.com>
2023-04-25 13:41:06 +0530

    updated on 25 april 23

--- server/index.js
+++ server/index.js
@@ -5,7 +5,12 @@ const express = require('express');
 const Movie = require('./Models/Movie')
 const multer = require('multer');
 const cors = require('cors')
-
+const { S3Client } = require('@aws-sdk/client-s3')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
+const fs = require('fs')
+const uuid = require("uuid").v4;
+const path = require("path")
 
 mongoose.connect('mongodb+srv://mini:WYm29wea1FVErStq@cluster0.2umezvv.mongodb.net/test', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
@@ -35,18 +40,34 @@ const storage = multer.diskStorage({
     cb(null, Date.now() + '-' + file.originalname)
   }
 });
+
+//s3
+
+const s3 = new aws.S3({
+  secretAccessKey: 'UTcE/pEQ4ejrvUg7jEWbIsrMi6f8gTJoD73DtWRR',
+  accessKeyId: 'AKIASMDJ64XXOW5OIIJE',
+  region: 'ap-south-1'
+});
+
+
 const upload = multer({ storage });
-
-
-// routes
 app.post('/api/movies', upload.single('poster'), async (req, res) => {
   try {
-    const result = await cloudinary.uploader.upload(req.file.path);
+    const fileContent = fs.readFileSync(req.file.path);
+
+    const params = {
+      Bucket: 'miniprobucket',
+      Key: `${Date.now()}-${req.file.originalname}`,
+      Body: fileContent,
+      ContentType: req.file.mimetype,
+      ACL: 'public-read',
+    };
+    const s3Data = await s3.upload(params).promise();
     const movie = new Movie({
       title: req.body.title,
       director: req.body.director,
       releaseYear: req.body.releaseYear,
-      poster: result.secure_url
+      poster: s3Data.Location,
     });
     await movie.save(); 
     res.send(movie);
@@ -55,6 +76,7 @@ app.post('/api/movies', upload.single('poster'), async (req, res) => {
     res.status(500).send('Something went wrong');
   }
 });
+
 
 app.get('/api/movies', async (req, res) => {
   try {
